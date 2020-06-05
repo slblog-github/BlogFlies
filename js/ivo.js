175630a6b1983845da5e64c3a4f3aff5af5e78b1
YUTONGWU <superxzlove@qq.com>
2020-06-05 15:42:09 +0800

    Upload JS(整合版本)

---
+++ js/ivo.js
@@ -0,0 +1,8 @@
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/slblog-github/BlogFlies/js/la.js\'>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/slblog-github/BlogFlies/js/piao.js\'>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/slblog-github/BlogFlies/js/fireworks.js\'>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/slblog-github/BlogFlies/js/ClickShowText.js\'>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/slblog-github/BlogFlies/js/click_heart.js\'>");
+document.writeln("<link type=\'text/css\' rel=\'stylesheet\' href=\'https://cdn.jsdelivr.net/gh/slblog-github/BlogFlies/js/dist/APlayer.min.css\'>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/slblog-github/BlogFlies/js/dist/APlayer.min.js\'>");
+document.writeln("<script src=\'https://cdn.jsdelivr.net/gh/slblog-github/BlogFlies/js/dist/music.js\'>");
